43d0c3a061617fd82821dfb41ea69a3127449dfc
Mike <miked7531@gmail.com>
2020-06-14 15:51:53 -0400

    basic js

---
+++ Basic Javascript Projects/js/Basic_Javascript_2.js
@@ -0,0 +1,12 @@
+function addNumbers(x,y)    //defining the function
+{
+    var result = x + y;
+    return result;
+}
+
+var sum = addNumbers(2,2);  //calling the function
+document.write(sum);
+
+function myfunction() {
+    document.getElementById("func").innerHTML = "2+2";
+}
---
+++ Basic Javascript Projects/js/basic_operator.js
@@ -0,0 +1,5 @@
+function myfunction() {
+    var sentence = "I am struggling";
+    sentence += " alot in javascript lol!";
+    document.getElementById("concatenate").innerHTML = sentence;
+}
