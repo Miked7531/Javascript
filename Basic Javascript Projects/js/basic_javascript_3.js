54dfda34541582181e504861970264e469babf72
Mike <miked7531@gmail.com>
2020-06-15 16:09:36 -0400

    pg 86

--- Basic Javascript Projects/js/basic_javascript_3.js
+++ Basic Javascript Projects/js/basic_javascript_3.js
@@ -8,3 +8,48 @@ function myfunction() {
     sentence += " alot in javascript lol!";
     document.getElementById("concatenate").innerHTML = sentence;
 }
+
+function subtraction_Function() {
+    var subtraction = 5 - 2;
+    document.getElementById("sub").innerHTML= "5 - 2 = " + subtraction;
+}
+
+function multiplication() {
+    var simple_math = 6 * 8;
+    document.getElementById("multi").innerHTML= "6 x 8 = " + simple_math;
+}
+
+function division() {
+    var simple_division = 48 / 6;
+    document.getElementById("div").innerHTML = " 48 / 6 = " + simple_division;
+}
+
+function more_math() {
+    var all_math = (1+2) * 10 /2 -5;
+    document.getElementById("all").innerHTML = "1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals " + all_math;
+}
+
+function modulus_Operator() {
+    var simple_remainder = 25%6;
+    document.getElementById("remainder").innerHTML="When you divide 25 by 6 you have a remainder of: " + simple_remainder;
+}
+
+function negation_Operator() {
+    var x= 10;
+    document.getElementById("unary").innerHTML= -x;
+}
+
+var x = 5;
+x++;
+document.write(x);
+
+var x = 5.25;
+x--;
+document.write(x);
+
+window.alert(Math.random());
+function pi_Function(){
+    document.getElementById("pi").innerHTML = Math.PI;
+}
+
+
